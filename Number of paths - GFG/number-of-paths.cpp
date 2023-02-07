e12eec72f3af796feadd4a09341444b267617b2c
RISHABH JAISWAL <85050354+Rishabh-code-do@users.noreply.github.com>
2023-02-07 14:57:45 +0530

    Added solution - LeetHub

--- Number of paths - GFG/number-of-paths.cpp
+++ Number of paths - GFG/number-of-paths.cpp
@@ -5,23 +5,19 @@ using namespace std;
 
 // } Driver Code Ends
 
+int count=0;
 int solve(int i,int j,int m,int n)
 {
-    if(i>m-1 || j>n-1)
-    return 0;
     if(i==m-1 && j==n-1)
     {
     return 1;
     }
-    int right=0;
-    int down=0;
-    if(i<=m-1)
-    right+=solve(i+1,j,m,n);
-    if(j<=n-1)
-    down+=solve(i,j+1,m,n);
-    
-    return right+down;
-    
+    if(i>m-1 || j>n-1)
+    return 0;
+    else 
+    {
+        return solve(i+1,j,m,n)+solve(i,j+1,m,n);
+    }
 }
 long long  numberOfPaths(int m, int n)
 {
